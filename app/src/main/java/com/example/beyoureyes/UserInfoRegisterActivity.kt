deb6326e02d1c529d5af88bad506cdcb8a21fc03
jordan-comlinee <leesoyoung0112@gmail.com>
2023-10-05 16:33:03 +0900

    registerActivity layout fixed

--- app/src/main/java/com/example/beyoureyes/UserInfoRegisterActivity.kt
+++ app/src/main/java/com/example/beyoureyes/UserInfoRegisterActivity.kt
@@ -7,19 +7,35 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class UserInfoRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info_register)
 
+
+        val chipGroup = findViewById<ChipGroup>(R.id.diseaseChipGroup)
+
+        chipGroup.setOnCheckedChangeListener { group, checkedId ->
+            val chip = findViewById<Chip>(checkedId)
+            if (chip != null) {
+                // 이 부분에서 선택된 Chip에 대한 작업을 수행할 수 있습니다.
+                val selectedText = chip.text.toString()
+                // 예: 선택된 Chip의 텍스트를 로그에 출력
+                println("선택된 Chip: $selectedText")
+            }
+        }
+
+
+
         val userIdClass = application as userId
         val userId = userIdClass.userId
 
         val db = Firebase.firestore
 
-        val testText : TextView = findViewById(R.id.test)
-
+        /*
         // 유저 정보 받아오기 - userId가 일치하는 경우에만!!
         db.collection("userInfo")
             .whereEqualTo("userID", userId)
@@ -34,6 +50,8 @@ class UserInfoRegisterActivity : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.w("FIRESTORE : ", "Error getting documents.", exception)
             }
+
+         */
 
 
     }
