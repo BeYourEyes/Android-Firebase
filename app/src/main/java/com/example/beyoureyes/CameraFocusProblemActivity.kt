d82f7ea161168cb50f0d14d68fa92c4fe077241f
Jordan <leesoyoung0112@gmail.com>
2023-12-11 20:12:12 +0900

    xml added

---
+++ app/src/main/java/com/example/beyoureyes/CameraFocusProblemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.beyoureyes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraFocusProblemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_focus_problem)
+    }
+}
--- app/src/main/java/com/example/beyoureyes/CameraOcrproblemActivity.kt
+++ app/src/main/java/com/example/beyoureyes/CameraOcrproblemActivity.kt
@@ -3,9 +3,9 @@ package com.example.beyoureyes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class FilmActivity : AppCompatActivity() {
+class CameraOcrproblemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_film)
+        setContentView(R.layout.activity_camera_ocrproblem)
     }
 }
